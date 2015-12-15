9767c75a1faef011a908fef85dc648afad03c860
Shun Gao <theodore.gao.1986@gmail.com>
2015-12-14 18:57:17 -0800

    boot_completed receiver

---
+++ sample-android/src/main/java/aero/panasonic/sample/android/BootCompletedReceiver.java
@@ -0,0 +1,15 @@
+package aero.panasonic.sample.android;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = BootCompletedReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.v(TAG, "onReceive() " + intent.getAction());
+    }
+}

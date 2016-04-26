42ae60cc6e3af123ce1592b3eb5f9f1cdf3d5f2b
Shun Gao <theodore.gao.1986@gmail.com>
2016-04-26 14:48:15 -0700

    add more widget sample

---
+++ sample-widget/widget-with-button-and-list/src/main/java/com/example/android/stackwidget/DummyActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.stackwidget;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class DummyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+}
---
+++ sample-widget/widget-with-button-and-list/src/main/java/com/example/android/stackwidget/DymmyReceiver.java
@@ -0,0 +1,18 @@
+package com.example.android.stackwidget;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DymmyReceiver extends BroadcastReceiver {
+    public DymmyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent activityIntent = new Intent(context, DummyActivity.class);
+        activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(activityIntent);
+    }
+}

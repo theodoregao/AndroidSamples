509bc4749ad7a4bb4c83590560a6fe4934de00d8
theodore.gao.1986@gmail.com <mdstwdh948>
2016-05-06 12:32:43 -0700

    class loader

--- sample-plugin/host/src/main/java/shun/gao/plugin/host/MainActivity.java
+++ sample-plugin/host/src/main/java/shun/gao/plugin/host/MainActivity.java
@@ -1,19 +1,32 @@
 package shun.gao.plugin.host;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+
+import dalvik.system.DexClassLoader;
 import shun.gao.plugin.sdk.CustomButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
 //    private AssetManager mAssetManager;
 //    private Resources mResources;
-//
+
 //    private String apkFileName = "sdk-debug.apk";
 //    private String dexpath = null;
 //    private File fileRelease = null;
@@ -22,29 +35,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
 //        dexpath = Environment.getExternalStorageDirectory() + File.separator + apkFileName;
 
+//        fileRelease = getDir("dex", 0);
 //        classLoader = new DexClassLoader(dexpath, fileRelease.getAbsolutePath(), null, getClassLoader());
 
 //        loadResources();
 
+//        Thread.currentThread().setContextClassLoader(classLoader);
+//        Looper.getMainLooper().getThread().setContextClassLoader(classLoader);
+//
+//        Log.e(TAG, CustomButton.class.getClassLoader().toString());
+
         try {
-            final Context otherContext = createPackageContext("shun.gao.plugin.resource", Context.CONTEXT_IGNORE_SECURITY);
+            final Context otherContext = createPackageContext("shun.gao.plugin.resource", Context.CONTEXT_IGNORE_SECURITY | CONTEXT_INCLUDE_CODE);
+            Log.e(TAG, "otherContent.getClasLoader(): " + otherContext.getClassLoader());
             View view = View.inflate(otherContext, shun.gao.plugin.sdk.R.layout.activity_main, null);
             setContentView(view);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-
-        CustomButton customButton = (CustomButton) findViewById(shun.gao.plugin.sdk.R.id.button);
-        customButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Custom Button onClick()", Toast.LENGTH_LONG).show();
-            }
-        });
     }
 
 //    private void loadResources() {
@@ -61,4 +73,30 @@ public class MainActivity extends AppCompatActivity {
 //        superRes.getConfiguration();
 //        mResources = new Resources(mAssetManager, superRes.getDisplayMetrics(), superRes.getConfiguration());
 //    }
+
+    private BroadcastReceiver CRITICAL_EVENT_RECEIVER = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (TextUtils.equals(intent.getAction(), getString(R.string.ACTION_CRITICAL_EVENT))) {
+                String params = intent.getStringExtra(getString(shun.gao.plugin.sdk.R.string.EXTRA_CRITICAL_EVENT));
+                Toast.makeText(MainActivity.this, "intent: " + intent + ", params: " + params, Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(getString(R.string.ACTION_CRITICAL_EVENT));
+//        LocalBroadcastManager.getInstance(this).registerReceiver(CRITICAL_EVENT_RECEIVER, intentFilter);
+        registerReceiver(CRITICAL_EVENT_RECEIVER, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        LocalBroadcastManager.getInstance(this).unregisterReceiver(CRITICAL_EVENT_RECEIVER);
+        unregisterReceiver(CRITICAL_EVENT_RECEIVER);
+    }
 }
--- sample-plugin/sdk/src/main/java/shun/gao/plugin/sdk/CustomButton.java
+++ sample-plugin/sdk/src/main/java/shun/gao/plugin/sdk/CustomButton.java
@@ -2,32 +2,74 @@ package shun.gao.plugin.sdk;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.support.v4.content.LocalBroadcastManager;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.Button;
 
 /**
  * Created by Theodore on 2016/5/5.
  */
 public class CustomButton extends Button {
+
+    private String mAttrsIntent;
+    private String mAttrsParams;
+
     public CustomButton(Context context) {
         super(context);
+        init();
     }
 
     public CustomButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setupAttributes(attrs);
+        init();
     }
 
     public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setupAttributes(attrs);
+        init();
     }
 
     @SuppressLint("NewApi")
     public CustomButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        setupAttributes(attrs);
+        init();
     }
 
     @Override
     public void setText(CharSequence text, BufferType type) {
         super.setText("CB:" + text, type);
+    }
+
+    private void setupAttributes(AttributeSet attrs) {
+        TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.CustomButton, 0, 0);
+
+        try {
+            mAttrsIntent = typedArray.getString(R.styleable.CustomButton_intent);
+            mAttrsParams = typedArray.getString(R.styleable.CustomButton_params);
+        } finally {
+            typedArray.recycle();
+        }
+    }
+
+    private void init() {
+        if (!TextUtils.isEmpty(mAttrsIntent)) {
+            setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setAction(mAttrsIntent);
+                    intent.putExtra(getResources().getString(R.string.EXTRA_CRITICAL_EVENT), mAttrsParams);
+//                    LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
+                    getContext().sendBroadcast(intent);
+                }
+            });
+        }
     }
 }

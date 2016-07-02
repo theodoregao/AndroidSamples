f77b2620bed500e014b1c44390afb4ac01cd1057
Shun Gao <theodore.gao.1986@gmail.com>
2016-07-01 22:26:26 -0700

    add nsd sample

---
+++ sample-nsd/nsd-consumer/src/main/java/aero/panasonic/nsdconsumer/NSDConsumer.java
@@ -0,0 +1,83 @@
+package aero.panasonic.nsdconsumer;
+
+import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.net.InetAddress;
+
+public class NSDConsumer extends AppCompatActivity {
+
+    private static final String TAG = NSDConsumer.class.getSimpleName();
+
+    private NsdManager.DiscoveryListener mDiscoveryListener = new NsdManager.DiscoveryListener() {
+        @Override
+        public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+            Log.d(TAG, "onStartDiscoveryFailed() " + errorCode);
+        }
+
+        @Override
+        public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+            Log.d(TAG, "onStopDiscoveryFailed() " + errorCode);
+        }
+
+        @Override
+        public void onDiscoveryStarted(String serviceType) {
+            Log.d(TAG, "onDiscoveryStarted() ");
+        }
+
+        @Override
+        public void onDiscoveryStopped(String serviceType) {
+            Log.d(TAG, "onDiscoveryStopped() ");
+        }
+
+        @Override
+        public void onServiceFound(NsdServiceInfo serviceInfo) {
+            Log.d(TAG, "onServiceFound() ");
+            NsdManager nsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+            nsdManager.resolveService(serviceInfo, mResolveListener);
+        }
+
+        @Override
+        public void onServiceLost(NsdServiceInfo serviceInfo) {
+            Log.d(TAG, "onServiceLost() ");
+        }
+    };
+
+    private NsdManager.ResolveListener mResolveListener = new NsdManager.ResolveListener() {
+
+        @Override
+        public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+            // Called when the resolve fails.  Use the error code to debug.
+            Log.e(TAG, "Resolve failed" + errorCode);
+        }
+
+        @Override
+        public void onServiceResolved(NsdServiceInfo serviceInfo) {
+            Log.e(TAG, "Resolve Succeeded. " + serviceInfo);
+
+            final TextView textView = (TextView) findViewById(R.id.text);
+            final String text = serviceInfo.getServiceName() + "\n" + serviceInfo.getHost();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(text);
+                }
+            });
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nsdconsumer);
+
+        NsdManager nsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+        nsdManager.discoverServices("_http._tcp.", NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
+    }
+}
---
+++ sample-nsd/nsd-provider/src/main/java/aero/panasonic/nsdprovider/NSDProvider.java
@@ -0,0 +1,81 @@
+package aero.panasonic.nsdprovider;
+
+import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.Formatter;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.util.Collections;
+import java.util.List;
+
+public class NSDProvider extends AppCompatActivity {
+
+    private static final String TAG = NSDProvider.class.getSimpleName();
+
+    private ServerSocket mServerSocket;
+    private NsdManager.RegistrationListener mRegistrationListener = new NsdManager.RegistrationListener() {
+        @Override
+        public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+            Log.d(TAG, "onRegistrationFailed() " + errorCode);
+        }
+
+        @Override
+        public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+            Log.d(TAG, "onUnregistrationFailed() " + errorCode);
+        }
+
+        @Override
+        public void onServiceRegistered(NsdServiceInfo serviceInfo) {
+            Log.d(TAG, "onServiceRegistered() ");
+        }
+
+        @Override
+        public void onServiceUnregistered(NsdServiceInfo serviceInfo) {
+            Log.d(TAG, "onServiceUnregistered() ");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nsdprovider);
+
+        try {
+            initializeNSD();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopNSD();
+    }
+
+    private void initializeNSD() throws IOException {
+        NsdServiceInfo nsdServiceInfo = new NsdServiceInfo();
+
+        nsdServiceInfo.setServiceName("Smartest Service");
+        nsdServiceInfo.setServiceType("_http._tcp.");
+        mServerSocket = new ServerSocket(0);
+        nsdServiceInfo.setPort(mServerSocket.getLocalPort());
+
+        NsdManager nsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+        nsdManager.registerService(nsdServiceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
+    }
+
+    private void stopNSD() {
+        NsdManager nsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+        nsdManager.unregisterService(mRegistrationListener);
+    }
+}

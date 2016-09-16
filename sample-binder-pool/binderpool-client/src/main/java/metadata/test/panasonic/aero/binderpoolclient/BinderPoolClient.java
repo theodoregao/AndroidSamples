86a144ffd39441cc1655d70e8bdbc5b11ed124f2
theodore.gao.1986@gmail.com <mdstwdh948>
2016-09-15 18:14:31 -0700

    update binder pool

---
+++ sample-binder-pool/binderpool-client/src/main/java/metadata/test/panasonic/aero/binderpoolclient/BinderPoolClient.java
@@ -0,0 +1,13 @@
+package metadata.test.panasonic.aero.binderpoolclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BinderPoolClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_binder_pool_client);
+    }
+}
---
+++ sample-binder-pool/binderpool-server/src/main/java/sg/shun/gao/binderpool/server/SecurityCenterImpl.java
@@ -0,0 +1,8 @@
+package sg.shun.gao.binderpool.server;
+
+/**
+ * Created by shun on 9/15/16.
+ */
+
+public class SecurityCenterImpl {
+}

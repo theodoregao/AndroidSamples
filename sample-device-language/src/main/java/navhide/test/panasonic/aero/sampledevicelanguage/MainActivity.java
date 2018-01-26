21bc19ce9f5a707f5452dd11062f56bcf2497525
Shun Gao <theodore.gao.1986@gmail.com>
2018-01-26 15:26:41 -0800

    sample for show locale language code.

---
+++ sample-device-language/src/main/java/navhide/test/panasonic/aero/sampledevicelanguage/MainActivity.java
@@ -0,0 +1,19 @@
+package navhide.test.panasonic.aero.sampledevicelanguage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText(Locale.getDefault().getISO3Language());
+    }
+}

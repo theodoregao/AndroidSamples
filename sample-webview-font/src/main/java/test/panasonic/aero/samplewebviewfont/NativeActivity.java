71f9bb44e966a36100afa0f4a9f295d6757806a1
theodore.gao.1986@gmail.com <mdstwdh948>
2018-04-18 14:55:43 -0700

    add more fonts.

--- sample-webview-font/src/main/java/test/panasonic/aero/samplewebviewfont/NativeActivity.java
+++ sample-webview-font/src/main/java/test/panasonic/aero/samplewebviewfont/NativeActivity.java
@@ -3,18 +3,27 @@ package test.panasonic.aero.samplewebviewfont;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.widget.TextView;
 
 public class NativeActivity extends AppCompatActivity {
+
+    private static final String TAG = NativeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_native);
 
-        TextView andalewtj = findViewById(R.id.text_andalewtj);
-        andalewtj.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/andalewtj.ttf"));
+//        TextView andalewtj = findViewById(R.id.text_andalewtj);
+//        Typeface andalewtjFont = Typeface.createFromAsset(getAssets(), "fonts/andalewtj.ttf");
+//
+//        if (andalewtjFont != null) andalewtj.setTypeface(andalewtjFont);
+//        else Log.e(TAG, "load font andalewtj failed.");
+
+        TextView tradbdo = findViewById(R.id.text_tradbdo);
+        tradbdo.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/tradbdo.ttf"));
 
         TextView chcry = findViewById(R.id.text_chcry);
         chcry.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/BLKCHCRY.TTF"));
